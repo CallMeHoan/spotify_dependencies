48b7c0fbd572e00ad5c1f4f3df50e06146e810b7
ThienHoanNeee <softteam2021@gmail.com>
2022-04-27 23:10:02 +0700

    [Update] update project package.json and add rules page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './styles.scss';
+import { ToastContainer } from 'react-toastify'
+import { Suspense, lazy } from 'react'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+
+const rules = lazy(() => import('./pages/rules'))
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <h1>Đạt Gà</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<Switch>
+					<Route component={rules} path='/rules' exact />
+				</Switch>
+				<ToastContainer
+					position='top-right'
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+				<ToastContainer />
+			</Suspense>
+		</Router>
+	)
 }
 
-export default App;
+export default App
---
+++ src/pages/rules/index.js
@@ -0,0 +1,256 @@
+import React from 'react'
+import './styles.scss'
+
+const rules = () => {
+	return (
+		<div className='rules'>
+			<span className='title'>Spotify Platform Rules</span>
+			<span className='text'>
+				Spotify’s mission is to unlock the potential of human creativity – by giving a million
+				creative artists the opportunity to live off their art and billions of fans the opportunity
+				to enjoy and be inspired by it. We believe achieving this mission on our platform is made
+				possible by welcoming different forms of artistic expression, ideas, perspectives, and
+				voices. This may mean that some content on our platform may not be to each individual’s
+				liking or is content that Spotify endorses.
+			</span>
+			<span className='text'>
+				However, that doesn’t mean that anything goes on our platform. In addition to the terms that
+				you agreed to governing your use of our services, these rules help ensure that everyone has
+				a safe and enjoyable experience.
+			</span>
+			<span className='title'>What are the rules?</span>
+			<span className='text'>
+				Whether you are a musician, podcaster, or other contributor, it is important to be aware of
+				what is not allowed on our platform. The examples in the below sections are for illustrative
+				purposes and are not exhaustive.
+			</span>
+			<span className='title'>Table of contents</span>
+			<ul>
+				<li>Dangerous Content</li>
+				<li>Deceptive Content</li>
+				<li>Sensitive Content</li>
+				<li>Illegal Content</li>
+				<li>What happens to rule breakers?</li>
+				<li>What else do I need to know?</li>
+				<li>How can I report an issue?</li>
+			</ul>
+			<span className='small__title'>Dangerous Content</span>
+			<span className='text'>
+				Spotify is home to communities where people can create, express themselves, listen, share,
+				learn, and be inspired. Don’t promote violence, incite hatred, harass or engage in any other
+				behavior that may place people at risk of serious physical harm or death. What to avoid:
+			</span>
+			<span className='text'>
+				Content that advocates or glorifies serious physical harm towards an individual or group
+				includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					encouraging, promoting, or glorifying suicide and self-harm (if you or someone you know is
+					struggling or thinking about self-harm, please see here for ways to get help)
+				</li>
+				<li>
+					inciting or threatening serious physical harm or acts of violence against a specific
+					target or specific group
+				</li>
+				<li>content that promotes or supports violent extremist organizations</li>
+			</ul>
+			<span className='text'>
+				Content that targets an individual or identifiable group for harassment or related abuse
+				includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>repeatedly targeting specific individuals with sexual advances</li>
+				<li>
+					sharing, threatening to share, or encouraging others to share someone’s private
+					information, including credit card or banking information, National Identity numbers, etc.
+				</li>
+			</ul>
+			<span className='text'>
+				Content that incites violence or hatred towards a person or group of people based on race,
+				religion, gender identity or expression, sex, ethnicity, nationality, sexual orientation,
+				veteran status, age, disability or other characteristics associated with systemic
+				discrimination or marginalization includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					praising, supporting, or calling for violence against a person or group of people based on
+					the characteristics listed above
+				</li>
+				<li>
+					dehumanizing statements about a person or group based on the protected characteristics
+					listed above
+				</li>
+				<li>promoting or glorifying hate groups and their associated images, and/or symbols</li>
+			</ul>
+			<span className='text'>
+				Content that promotes dangerous false or dangerous deceptive medical information that may
+				cause offline harm or poses a direct threat to public health includes, but may not be
+				limited to:
+			</span>
+			<ul>
+				<li>
+					asserting that AIDS, COVID-19, cancer or other serious life threatening diseases are a
+					hoax or not real
+				</li>
+				<li>
+					encouraging the consumption of bleach products to cure various illnesses and diseases
+				</li>
+				<li>
+					promoting or suggesting that vaccines approved by local health authorities are designed to
+					cause death
+				</li>
+				<li>
+					encouraging people to purposely get infected with COVID-19 in order to build immunity to
+					it (e.g. promoting or hosting “coronavirus parties”)
+				</li>
+			</ul>
+			<span className='text'>
+				Content that illicitly promotes the sale of regulated or illegal goods includes, but may not
+				be limited to:
+			</span>
+			<ul>
+				<li>selling illegal firearms or firearm parts</li>
+				<li>selling illegal drugs</li>
+				<li>selling endangered species or products derived from endangered species</li>
+			</ul>
+			<span className='text'>
+				Content that promotes, solicits, or facilitates child sexual abuse or exploitation includes,
+				but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					visual depictions of a minor engaged in a sexual act or lascivious depictions of a nude
+					minor
+				</li>
+				<li>promoting acts of sexual abuse against a child in exchange for money</li>
+				<li>encouraging or promoting sexual attraction by adults towards minors</li>
+				<li>promoting, normalizing, or glorifying child grooming behaviors</li>
+			</ul>
+			<span className='small__title'>Deceptive Content</span>
+			<span className='text'>
+				Creating great experiences on Spotify requires trust that people are who they say they are,
+				that they won’t be scammed, and that no one is trying to manipulate our platform. Don’t use
+				malicious practices to deceive others. What to avoid:
+			</span>
+			<span className='text'>
+				Content that impersonates others in order to deceive includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>replicating the same name, image, and/or description as another existing creator</li>
+				<li>posing as another person, brand, or organization in a misleading manner</li>
+			</ul>
+			<span className='text'>
+				Content that promotes manipulated and synthetic media as authentic in ways that pose the
+				risk of harm includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					audio or video recording that comes from a real and valid source that has been altered in
+					a way that changes the meaning or context of the original media and is purported to be
+					true, thus posing a risk of harm to the speaker or other individuals
+				</li>
+				<li>
+					audio or visual media artificially created through the use of technology that’s purported
+					to be true, such as digitally manufactured sexual audio and video content or content
+					falsely suggesting that someone committed a crime
+				</li>
+			</ul>
+			<span className='text'>
+				Content that attempts to manipulate or interfere with election-related processes includes,
+				but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					misrepresentation of procedures in a civic process that could discourage or prevent
+					participation
+				</li>
+				<li>
+					misleading content promoted to intimidate or suppress voters from participating in an
+					election
+				</li>
+			</ul>
+			<span className='text'>
+				Content that attempts to take advantage of the Spotify community includes, but may not be
+				limited to:
+			</span>
+			<ul>
+				<li>
+					posting, sharing, or providing instructions on implementing malware or related malicious
+					practices that seek to harm or gain unauthorized access to computers, networks, systems,
+					or other technologies
+				</li>
+				<li>
+					phishing or related attempts to deceptively solicit or collect sensitive information
+				</li>
+				<li>
+					promoting investment and financial scams like get rich quick and pyramid schemes, or
+					otherwise encouraging others to part with money under false pretenses
+				</li>
+			</ul>
+			<span className='small__title'>Sensitive Content</span>
+			<span className='text'>
+				We have tons of amazing content on Spotify, but there are certain things that we don’t allow
+				on our platform. Don’t post excessively violent or graphic content, and don’t post sexually
+				explicit content. What to avoid:
+			</span>
+			<span className='text'>
+				Content that promotes graphic or gratuitous depictions of violence, gore, or other shocking
+				imagery includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>severely mutilated or dismembered bodies</li>
+				<li>promoting animal cruelty or torture</li>
+			</ul>
+			<span className='text'>
+				Content that contains sexually explicit material includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>
+					pornography or visual depictions of genitalia or nudity presented for the purpose of
+					sexual gratification
+				</li>
+				<li>advocating or glorifying sexual themes related to rape, incest, or beastiality</li>
+			</ul>
+			<span className='small__title'>Illegal Content</span>
+			<span className='text'>
+				The law is the law. No matter who you are, it is your responsibility to comply with
+				applicable laws and regulations. What to avoid:
+			</span>
+			<span className='text'>
+				Content that violates applicable laws and regulations includes, but may not be limited to:
+			</span>
+			<ul>
+				<li>content that does not comply with applicable sanctions and export regulations</li>
+				<li>content that is intended to promote or commit an illegal act of any kind</li>
+			</ul>
+			<span className='text'>
+				Content that infringes the intellectual property rights of others includes, but may not be
+				limited to:
+			</span>
+			<ul>
+				<li>content provided to Spotify without obtaining necessary permissions</li>
+				<li>content that infringes third-party copyrights or trademarks</li>
+			</ul>
+			<span className='title'>What happens to rule breakers?</span>
+			<span className='text'>
+				We take these decisions seriously and keep context in mind when making them. Breaking the
+				rules may result in the violative content being removed from Spotify. Repeated or egregious
+				violations may result in accounts being suspended and/or terminated.
+			</span>
+			<span className='title'>What else do I need to know?</span>
+			<span className='text'>
+				These platform rules will help ensure that Spotify remains an open and safe platform for
+				all. We will continue to evaluate and update this information as needed, so please check
+				back frequently. Depending on which Spotify products or features you use, you may be subject
+				to additional requirements so please follow those too.
+			</span>
+			<span className='title'>How can I report an issue?</span>
+			<span className='text'>
+				Have you found an issue with a piece of content on Spotify? If so please let us know by
+				reporting it.
+			</span>
+		</div>
+	)
+}
+export default rules
